ece8c321dfe240da43f89ee7b2b9e8a8629151dd
winerss <2857505905@qq.com>
2018-11-04 16:42:58 +0800

    20181104

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,4 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // API_ROOT: '"https://www.dadan299.com"'
-  API_ROOT: '"http://www.huangjb.net"'
 })
